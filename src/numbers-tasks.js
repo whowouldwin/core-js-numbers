6baad50be8cb57cbb287110d49b6447e83753d2e
whowouldwin <leranetwork@gmail.com>
2024-12-14 01:31:28 +0300

    feat: implement getIntegerOnString, isSafeInteger, roundToSmallestInteger, roundToLargestInteger, getIntegerPartNumber, getSumOfNumbers, getMaxNumber, getRandomInteger, getHypotenuse, getCountOfOddNumbers

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -634,8 +634,16 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  const value = Math.abs(number);
+  let count = 0;
+  if (value <= 0) return 0;
+  for (let i = 1; i <= value; i += 1) {
+    if (i % 2 !== 0) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
